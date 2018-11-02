dc4e827e0d19a3918b3de6629955cdcc2c4afa34
THOUFEEQ HIDAYATH <77thoufeeq77@gmail.com>
2018-11-02 15:44:39 +0530

    Create hell.java

---
+++ src/main/java/com/aws/codestar/projecttemplates/controller/hell.java
@@ -0,0 +1,25 @@
+package com.aws.codestar.projecttemplates.controller;
+
+import org.json.JSONObject;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Basic Spring web service controller that handles all GET requests.
+ */
+@RestController
+@RequestMapping("/")
+public class HelloWorldController {
+
+    private static final String MESSAGE_FORMAT = "Hello %s!";
+
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    public String helloWorldGet(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "hello";
+    }
+
+   
+}
